0ef835585fac7e23302e3f42d2092f5636e6316f
BinhMinhFPLHN <binhminhchun12@gmail.com>
2020-04-17 20:38:14 +0700

    2 files modified, 2 added

---
+++ src/app/product.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import{ data } from './MockData';
+import{ Product } from './roduct';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  api = 'https://5e79ba7017314d0016133563.mockapi.io/product';
+  // products=data;
+  constructor(
+     private http: HttpClient
+  ) { }
+ 
+
+ getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.api);
+    // return this.products;
+  }
+  getProduct(id): Observable<Product>{
+     console.log(id);
+    return this.http.get<Product>(`${this.api}/${id}`);
+    // return this.products;
+  }
+  addProduct(product): Observable<Product>{
+    return this.http.post<Product>(`${this.api}`, product);
+    // let newObj = { id: 6, ...product };
+    // this.products.push(newObj);
+  }
+  updateProduct(product): Observable<Product>{
+    return this.http.put<Product>(`${this.api}/${product.id}`, product);
+  }
+
+}
+
+
