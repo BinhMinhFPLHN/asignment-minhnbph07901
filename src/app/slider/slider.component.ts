4f3f6d5ea14ea323b96ba1f4b70d87f68a7b0d26
BinhMinhFPLHN <binhminhchun12@gmail.com>
2020-04-18 15:13:03 +0700

    6 files modified

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-slide',
+  selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
