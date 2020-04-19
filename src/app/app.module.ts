ca806def0163ac9fd82c3fcf108d12376406e2c5
BinhMinhFPLHN <binhminhchun12@gmail.com>
2020-04-19 21:27:06 +0700

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,7 @@ import { DashboardManagerComponent } from './dashboard-manager/dashboard-manager
 
 
 @NgModule({
-  imports:      [ AppRoutingModule, BrowserModule, FormsModule,CarouselModule ],
+  imports:      [ AppRoutingModule, BrowserModule, FormsModule,CarouselModule, NgbModule ],
   declarations: [ AppComponent, HelloComponent, SlideComponent, ProductGTComponent, ProductMenuComponent, ProductListComponent, HomeComponent, AdminComponent, DashboardComponent, DashboardManagerComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/dashboard-manager/dashboard-manager.component.ts
+++ src/app/dashboard-manager/dashboard-manager.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { data } from '../MockData';
 @Component({
   selector: 'app-dashboard-manager',
   templateUrl: './dashboard-manager.component.html',
   styleUrls: ['./dashboard-manager.component.css']
 })
 export class DashboardManagerComponent implements OnInit {
-
+  items = data;
   constructor() { }
 
   ngOnInit() {
