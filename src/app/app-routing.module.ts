5a2b7cd08f9f72d2e7a178b9de2f67607a48f3b9
BinhMinhFPLHN <binhminhchun12@gmail.com>
2020-04-21 14:59:15 +0700

    2 files modified, 3 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductGTComponent } from './product-gt/product-gt.component';
 import { ProductMenuComponent } from './product-menu/product-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -30,7 +29,7 @@ const routes: Routes = [
   { path: 'product-gt', component: ProductGTComponent},
   { path: 'new', component: NewComponent},
   { path: 'menu', component: ProductComponent},
-  {path: 'product-list', component: ProductListComponent},
+
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HelloComponent } from './hello.component';
 import { SlideComponent } from './slider/slider.component';
 import { ProductGTComponent } from './product-gt/product-gt.component';
 import { ProductMenuComponent } from './product-menu/product-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -18,6 +17,7 @@ import { DashboardManagerComponent } from './dashboard-manager/dashboard-manager
 import { DashboardAddComponent } from './dashboard-add/dashboard-add.component';
 import { ProductComponent } from './product/product.component';
 import { NewComponent } from './new/new.component';
+import { DashboardEditComponent } from './dashboard-edit/dashboard-edit.component';
 
 
 
@@ -26,7 +26,7 @@ import { NewComponent } from './new/new.component';
 
 @NgModule({
   imports:      [ AppRoutingModule, BrowserModule, FormsModule,CarouselModule, NgbModule ],
-  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductGTComponent, ProductMenuComponent, ProductListComponent, HomeComponent, AdminComponent, DashboardComponent, DashboardManagerComponent, DashboardAddComponent, ProductComponent, NewComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductGTComponent, ProductMenuComponent, HomeComponent, AdminComponent, DashboardComponent, DashboardManagerComponent, DashboardAddComponent, ProductComponent, NewComponent, DashboardEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
